644b5f63bbc9ae6ff0a64dc32db4d46d1a481ec4
srinivas cheerla <chsreenivas92@gmail.com>
2020-09-21 22:14:52 +0530

    Equilibrium index in an array

--- mustdocodingquestions/arrays/EquilibriumPoint.java
+++ mustdocodingquestions/arrays/EquilibriumPoint.java
@@ -2,6 +2,31 @@ package mustdocodingquestions.arrays;
 
 import java.util.Arrays;
 
+/**
+ * https://practice.geeksforgeeks.org/problems/equilibrium-point/0
+ * 
+ * Given an array A of N positive numbers. The task is to find the position 
+ * where equilibrium first occurs in the array. Equilibrium position in an array 
+ * is a position such that the sum of elements before it is equal to the sum of 
+ * elements after it.
+ * 
+ * Input:
+2
+1
+1
+5
+1 3 5 2 2
+
+Output:
+1
+3
+
+Explanation:
+Testcase 1: Since its the only element hence its the only equilibrium point.
+Testcase 2: For second test case equilibrium point is at position 3 as elements 
+below it (1+3) = elements after it (2+2).
+ */
+
 public class EquilibriumPoint {
     public static void main(String[] args){
         int[] input = new int[]{1,2,3,4,5};
